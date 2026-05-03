41aa9c2fbfeaf80112189d63f6e9fdcc5d413a2b
= <larryking8118@gmail.com>
2026-05-03 17:00:31 +0000

    feat(toasts): configure toast slice and add to store

---
+++ src/store/features/toast/toast.selectors.js
@@ -0,0 +1,1 @@
+export const selectToastVisibility = (state) => state.toast.visible
--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from './features/posts/posts.slice'
 import usersReducer from './features/users/users.slice'
 import commentsReducer from './features/comments/comments.slice'
+import toastReducer from './features/toast/toast.sclice'
+
+
 
 
 
@@ -9,7 +12,8 @@ const store = configureStore({
     reducer: {
         posts: postsReducer,
         users: usersReducer,
-        comments: commentsReducer
+        comments: commentsReducer,
+        toast: toastReducer
     }
 })
 
