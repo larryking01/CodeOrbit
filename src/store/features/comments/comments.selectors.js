017c0d90f2c0b65281c9761b58254075954c18f9
= <larryking8118@gmail.com>
2026-05-06 15:06:25 +0000

    refactor(selectors): memoize comment selection by postId, memoize number of comments selection to prevent unnecessary re-renders and reset comment content after attempted create

--- src/store/features/comments/comments.selectors.js
+++ src/store/features/comments/comments.selectors.js
@@ -1,12 +1,38 @@
-export const selectCommentByPostId = (state, postId) =>  {
-    let comments = state.comments.comments.filter(comment => comment.postId == postId)
+import { createSelector } from "@reduxjs/toolkit"
+
+
+
+
+
+export const selectAllComments = (state) => {
+    let comments = state.comments.comments 
     return comments
 }
 
 
-export const selectNumberOfComments = (state, postId) => {
-    let comments = state.comments.comments.filter(comment => comment.postId == postId)
-    return comments.length
-}
+// memoize selector to prevent unnecessary re-renders
+export const selectCommentByPostId = createSelector(
+    [
+        selectAllComments,
+        (state, postId) => postId 
+    ],
+    (comments, postId) => {
+        let filteredComments = comments.filter(comment => comment.postId == postId)
+        return filteredComments
+    }
+)
+
+
+// memoize selector to prevent unnecessary re-renders
+export const selectNumberOfComments = createSelector(
+    [
+        selectAllComments,
+        (state, postId) => postId
+    ],
+    (comments, postId) => {
+        let filteredComments = comments.filter(comment => comment.postId == postId)
+        return filteredComments.length
+    }
+)
 
 
